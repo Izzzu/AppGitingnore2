a53fa60be15540ad8177ed9cd0a8e5aae85caa00
Iza <izabel.kulak@gmail.com>
2017-11-04 15:29:45 +0100

    Main change

--- src/sda/Main.java
+++ src/sda/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("To jest projekt do testowania ignorowania plików w gicie.");
+		System.out.println("Cos tam");
 
 	}
 }
